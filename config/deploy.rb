7b0ba938d1025482ba80de6cc04732572bf9bbe6
Thomas Marek <thomas_marek@gmx.net>
2014-06-04 20:16:32 +0200

    Rubber: Configure DigitalOcean deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,9 +12,9 @@ end
 
 # Use a simple directory tree copy here to make demo easier.
 # You probably want to use your own repository for a real app
-set :scm, :none
-set :repository, "."
-set :deploy_via, :copy
+set :scm, :git
+set :repository, "https://github.com/tmak/word_games.git"
+set :deploy_via, :remote_cache
 
 # Easier to do system level config as root - probably should do it through
 # sudo in the future.  We use ssh keys for access, so no passwd needed
